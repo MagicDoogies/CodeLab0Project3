50bd26fd45bd79afe65af9546e822fe621b2de3b
zeeodd <evergreenskier@gmail.com>
2019-11-04 19:41:10 -0500

    1. Added gameobjects to my scene 2. First draft of CameraShake.cs

---
+++ CodeLab0UnityProject3/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.localPosition = new Vector3(Random.Range(-1, 1), Random.Range(-1, 1) + 15, Random.Range(-1, 1) - 20) * 0.3f;
+    }
+}
