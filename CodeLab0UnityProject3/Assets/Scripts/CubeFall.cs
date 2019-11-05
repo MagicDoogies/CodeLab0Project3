25edc74f12dc46b837db9891e646dae70ae6bc52
zeeodd <evergreenskier@gmail.com>
2019-11-05 07:50:55 -0500

    1. Added CubeFall.cs to control the cube 2. Initialized a basic version of CubeFall.cs

---
+++ CodeLab0UnityProject3/Assets/Scripts/CubeFall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeFall : MonoBehaviour
+{
+    // Get the scene's camera
+    public Camera mainCamera;
+
+    // Initialize an rb var to control gravity
+    Rigidbody rb;
+
+    void Start()
+    {
+        // Set use of gravity to false at start
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
+    void Update()
+    {
+        // Have the cube fall if space key is pressed
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!rb.useGravity)
+            {
+                rb.useGravity = true;
+            }
+        }
+    }
+}
